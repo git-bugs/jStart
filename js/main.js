da4c679ac98b3177ee6ae79fb4ac7dfa5202c594
git-bugs <voov3r@gmail.com>
2020-10-09 11:14:23 +0300

    hrd-less2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-let money,income,addExpenses,deposit,mission,period;
-alert('Q');
-console.log('qQ');
+const num = 266219;
+
+const str = num.toString();
+let result = 1;
+
+for (let i = 0; i<str.length; i++){
+  result *= str[i];
+};
+
+console.log(result);
+
+result **= 3;
+
+console.log(result.toString()[0]);
+console.log(result.toString()[1]);
