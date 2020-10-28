0635329d00e5cd76b568fbfd8611ae176c84cd59
git-bugs <voov3r@gmail.com>
2020-10-28 11:40:31 +0300

    l15-2

---
+++ main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+class First {
+  hello() {
+    console.log('Привет я метод родителя!');
+  }
+}
+
+class Second extends First {
+  hello() {
+    super.hello();
+    console.log('А я наследуемый метод!');
+  }
+}
+
+
+let newSecond = new Second();
+
+newSecond.hello();
